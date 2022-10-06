421088eabf19765eda12e15f8e5ddfebad6a9f62
Niranjankumar7 <43720695+niranjankumar7@users.noreply.github.com>
2022-10-06 17:40:24 +0530

    menu working fine
    menu retriving data properly. need to give proper service for branch so that it can retrive the data from users n display in table. Still have to add edit n delete functions

---
+++ services/menu.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  
+  constructor(private http : HttpClient) { 
+   
+   }
+   getmenu(){
+    return this.http.get("http://localhost:8080/foodProduct");
+   }
+}
