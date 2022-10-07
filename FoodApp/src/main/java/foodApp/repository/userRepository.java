346f32182b475300b99f94a139969febeb58f36d
Harsh00721 <81928388+Harsh00721@users.noreply.github.com>
2022-10-07 15:30:12 +0530

    Delete FoodApp directory

--- FoodApp/src/main/java/foodApp/repository/userRepository.java
+++
@@ -1,9 +0,0 @@
-package foodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import foodApp.dto.user;
-
-public interface userRepository extends JpaRepository<user, Integer> {
-
-}
